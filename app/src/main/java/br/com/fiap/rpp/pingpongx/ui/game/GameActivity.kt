4296467f91bde60b3842c8a5c49d316fd0aa1442
Logon Aluno <logonaluno@local.com>
2020-01-29 23:06:19 -0300

    Listener e observers.

--- app/src/main/java/br/com/fiap/rpp/pingpongx/ui/game/GameActivity.kt
+++ app/src/main/java/br/com/fiap/rpp/pingpongx/ui/game/GameActivity.kt
@@ -2,7 +2,7 @@ package br.com.fiap.rpp.pingpongx.ui.game
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.com.fiap.rpp.pingpongx.R
 import kotlinx.android.synthetic.main.activity_game.*
 
@@ -14,20 +14,50 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        mGameViewModel = GameViewModel()
+        //mGameViewModel = GameViewModel()
+        //mGameViewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        mGameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         val players = intent.extras?.get("players") as HashMap<*, *>
 
-        if (players.isNotEmpty() ) {
+        if (players.isNotEmpty()) {
 
             playerOneLabelID.text = players["p1"].toString()
             playerTwoLabelID.text = players["p2"].toString()
 
         }
 
-        mGameViewModel.getCurrentPlayerOneScoreLiveData().observeForever(Observer {})
+        // Initial
+        if (mGameViewModel.getCurrentPlayerOneScoreLiveData().value != null) {
+            playerOneScoreID.text = mGameViewModel.getCurrentPlayerOneScoreLiveData().value.toString()
+        }
+        if (mGameViewModel.getCurrentPlayerTwoScoreLiveData().value != null) {
+            playerTwoScoreID.text = mGameViewModel.getCurrentPlayerTwoScoreLiveData().value.toString()
+        }
 
-        mGameViewModel.getCurrentPlayerTwoScoreLiveData().observeForever(Observer {})
+
+        mGameViewModel.getCurrentPlayerOneScoreLiveData().observeForever {
+            it as Int
+            playerOneScoreID.text = it.toString()
+        }
+
+        mGameViewModel.getCurrentPlayerTwoScoreLiveData().observeForever {
+            it as Int
+            playerTwoScoreID.text = it.toString()
+        }
+
+        playerOneSumOneID.setOnClickListener {
+
+            var currentScore = playerOneScoreID.text.toString().toInt()
+            mGameViewModel.getCurrentPlayerOneScoreLiveData().value = ++currentScore
+        }
+
+        playerTwoSumOneID.setOnClickListener {
+
+            var currentScore = playerTwoScoreID.text.toString().toInt()
+            mGameViewModel.getCurrentPlayerTwoScoreLiveData().value = ++currentScore
+
+        }
 
     }
 }
